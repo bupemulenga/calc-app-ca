22c93bb1b523ef5751e63bb69a5283168be29caf
ICT - BUPE MULENGA <mulengbu@zra.org.zm>
2023-08-25 12:52:31 +0200

    Add interactivity to the calculator using Vanilla JS

---
+++ script.js
@@ -0,0 +1,98 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector(".screen");
+
+// Control the button click action by the user
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
+}
+
+// handle the symbol that is clicked by the user
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = 0;
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+
+    default:
+      break;
+  }
+}
+
+// handle the
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === 0) {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
